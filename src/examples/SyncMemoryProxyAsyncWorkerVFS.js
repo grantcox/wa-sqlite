5ce1bea4b5c680e5328e70aacba7dea837ed5e16
Grant Cox <grantc@ynab.com>
2025-05-13 12:10:25 +1000

    Overriding the #makeDataArray inherited from the FacadeVFS to drop the Proxy(), much faster!

--- src/examples/SyncMemoryProxyAsyncWorkerVFS.js
+++ src/examples/SyncMemoryProxyAsyncWorkerVFS.js
@@ -451,4 +451,14 @@ export class SyncMemoryProxyAsyncWorkerVFS extends FacadeVFS {
     pResOut.setInt32(0, fileExists || isBufferedDb ? 1 : 0, true);
     return VFS.SQLITE_OK;
   }
+  
+  /**
+   * Override the base makeDataArray to not watch for WebAssembly memory resize.
+   * as we always use the memory immediately.
+   * @param {number} byteOffset 
+   * @param {number} byteLength 
+   */
+    makeDataArray(byteOffset, byteLength) {
+      return this._module.HEAPU8.subarray(byteOffset, byteOffset + byteLength);
+    }
 }
